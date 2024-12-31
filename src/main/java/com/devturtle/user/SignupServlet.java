42e608a0f9b706854056d67cd4f4785fb9344a9a
백종현 <89888855+sir-Crab@users.noreply.github.com>
2024-12-31 20:02:54 +0900

    [chore] 회원가입 로직 수도 작성
    회원가입 로직 수도 작성

--- src/main/java/com/devturtle/user/SignupServlet.java
+++ src/main/java/com/devturtle/user/SignupServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.devturtle.rank.RankUserDAO;
 import com.google.gson.Gson;
 
 @WebServlet("/signup")
@@ -20,7 +21,12 @@ public class SignupServlet extends HttpServlet {
         String joiningStr = request.getReader().lines().collect(Collectors.joining());
         System.out.println("Received JSON: " + joiningStr);
 
-
+//        UserDAO udao = new UserDAO();
+//        RankUserDAO rdao = new RankUserDAO();
+//        
+//        if(udao.insertUser(uvo) == 1) {
+//        	rdao.insertRankUser(udao.selectUserByLoginID(uvo.getLoginID).getUserID(), 0);
+//        }
         // 클라이언트에게 로그인 페이지로 리다이렉트
         request.getRequestDispatcher(joiningStr).forward(request, response);
     }
