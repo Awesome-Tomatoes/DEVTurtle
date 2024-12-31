e0c8d856a2d823f2e2cf07cd626ea4ba1c2f0bf4
백종현 <89888855+sir-Crab@users.noreply.github.com>
2024-12-31 10:36:19 +0900

    [chore] 프로젝트 옮김

---
+++ src/main/java/com/devturtle/group/GroupDAO.java
@@ -0,0 +1,24 @@
+package com.devturtle.group;
+import java.util.ArrayList;
+
+public class GroupDAO {
+
+
+	// 한그룹에 대한 정보
+	public GroupVO select(int groupID) {	
+		return new GroupVO(groupID,"멋쟁이그룹","설명","스터디","공개",50000,60,"2024-12-12","2024-12-12"); 
+	}
+	
+	// 전체그룹 리스트 정보
+	public ArrayList<GroupVO> selectAll() {
+		
+		ArrayList<GroupVO> alist = new ArrayList<GroupVO>();
+
+		alist.add(new GroupVO(1,"group1","설명1","스터디","공개",50000,60,"2024-12-12","2024-12-12"));
+		alist.add(new GroupVO(2,"group2","설명2","스터디","공개",40000,70,"2024-12-12","2024-12-12"));
+		alist.add(new GroupVO(3,"group3","설명3","스터디","공개",60000,50,"2024-12-12","2024-12-12"));
+		alist.add(new GroupVO(4,"group4","설명4","스터디","공개",80000,30,"2024-12-12","2024-12-12"));	
+		
+		return alist;
+	}
+}
---
+++ src/main/java/com/devturtle/main/MainServlet.java
@@ -0,0 +1,38 @@
+package com.devturtle.main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.DevTurtle.user.UserDAO;
+import com.DevTurtle.user.UserVO;
+
+@WebServlet("/main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MainServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UserDAO udao = new UserDAO();
+		ArrayList<UserVO> ulist = udao.select();
+		
+		request.setAttribute("ulist", ulist);
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/sb/board_list.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
