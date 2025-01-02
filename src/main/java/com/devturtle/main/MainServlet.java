548478ca36450a540447a879914f0345300c24a3
Hoshi03 <ichanho114@gmail.com>
2025-01-02 20:44:05 +0900

    [feat] 개인/그룹 랭크 상위 6등까지 뽑는 로직 메인에 추가
    개인/그룹 랭크 상위 6등까지 뽑는 로직 메인에 추가

--- src/main/java/com/devturtle/main/MainServlet.java
+++ src/main/java/com/devturtle/main/MainServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.devturtle.group.GroupDAO;
 import com.devturtle.group.GroupVO;
+import com.devturtle.user.UserDAO;
+import com.devturtle.user.UserVO;
 
 @WebServlet("/main")
 public class MainServlet extends HttpServlet {
@@ -21,7 +23,7 @@ private static final long serialVersionUID = 1L;
     }
 
 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//	UserDAO udao = new UserDAO();
+	UserDAO udao = new UserDAO();
 //	ArrayList<UserVO> ulist = udao.selectUser(1);
 //	request.setAttribute("ULIST", ulist);
 	
@@ -36,7 +38,19 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response) t
 
     // layout.jsp로 포워딩
     request.getRequestDispatcher("/index.jsp").forward(request, response);
-	
+    
+//	  //랭킹뽑기
+//    //그룹 상위 1~6위
+//	ArrayList<GroupVO> groupList = gdao.selectAllGroupByMonthOrderByRankPaging("20250102", 1, 6);
+//	for(var x : groupList) {
+//		System.out.println(x.toString());
+//	}
+//	
+//	//개인 상위 1~6위
+//	ArrayList<UserVO> userList = udao.selectAllUserByMonthOrderByRankPaging("20241231", 1, 6);
+//	for(var x : userList) {
+//		System.out.println(x.toString());
+//	}
 	
 }
 
