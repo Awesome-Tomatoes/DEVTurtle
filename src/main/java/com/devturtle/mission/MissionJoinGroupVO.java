50945d5238a55deaa97e7104bcb798901d40872a
sk-choi <victory9807@gmail.com>
2025-01-02 23:31:34 +0900

    [feat] MissionJoinGroupVO 추가

---
+++ src/main/java/com/devturtle/mission/MissionJoinGroupVO.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.devturtle.mission;
+
+/**
+ * Description : 클래스에 대한 설명을 입력해주세요.<br>
+ * Date : 2025. 1. 2.<br>
+ * History :<br>
+ * - 작성자 : sk-choi, 날짜 : 2025. 1. 2., 설명 : 최초작성<br>
+ *
+ * @author sk-choi
+ * @version 1.0
+ */
+
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MissionJoinGroupVO {
+	// Objective와 Groups를 join한 정보를 담는 VO
+	
+	private int group_id;
+	private int objective_id;
+	private String gname;
+	private String contents;
+	private String success_date;
+	private int points;
+	private int count;
+	
+
+}
