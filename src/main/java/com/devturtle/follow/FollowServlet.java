09bbd0218a2120d8f3360c50eda19188e5d726be
백종현 <pigmammam4@gmail.com>
2025-01-05 21:19:23 +0900

    [fix] follow page parameter bug fix

--- src/main/java/com/devturtle/follow/FollowServlet.java
+++ src/main/java/com/devturtle/follow/FollowServlet.java
@@ -29,10 +29,13 @@ public class FollowServlet extends HttpServlet {
 		FollowDAO fdao = new FollowDAO();
 		UserDAO udao = new UserDAO();
 		
-		int userid = Integer.parseInt(request.getParameter("userid"));
+		String useridStr = request.getParameter("userid");
+		int userid = 0;
 		
-		if (userid == 0) {
+		if (useridStr == null || useridStr == "") {
 			userid = (int) session.getAttribute("SESS_USER_ID");
+		} else {
+			userid = Integer.parseInt(useridStr);
 		}
 		
 		ArrayList<UserVO> followedlist = fdao.selectAllFollowed(userid);
