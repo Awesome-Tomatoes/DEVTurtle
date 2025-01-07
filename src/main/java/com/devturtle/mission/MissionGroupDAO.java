80de49ec6acbf052aa30f5d29f64c29553d0fa21
sk-choi <victory9807@gmail.com>
2025-01-07 22:50:11 +0900

    [chore] MissionGroupDAO에 그룹미션번호 조회 기능 추가

--- src/main/java/com/devturtle/mission/MissionGroupDAO.java
+++ src/main/java/com/devturtle/mission/MissionGroupDAO.java
@@ -39,18 +39,43 @@ public class MissionGroupDAO {
 		return uov;
 	}
 	
-	public ArrayList<ObjectiveVO> selectAll() {
+	public ArrayList<ObjectiveVO> selectAllMission() {
 		
 		ArrayList<ObjectiveVO> ulist = new ArrayList<ObjectiveVO>();
 		
-		ObjectiveVO uov1 = new ObjectiveVO(1, "dsds", "dsdsd","파일 압축하고 풀기 100번 달성!", 100);
-	    ObjectiveVO uov2 = new ObjectiveVO(2, "sdsd", "saaaa","폴더 지웠다 다시 만들기 1000번 달성!", 1000);
-
-	    ulist.add(uov1);
-	    ulist.add(uov2);
-		
 		return ulist;
 	}
+	
+	
+	
+	// 모든 그룹 미션 조회
+	public ArrayList<Integer> selectAllMissionGroupNum() {
+		
+		DBManager o = OracleDBManager.getInstance();
+		
+		ArrayList<Integer> mlist = new ArrayList<Integer>();
+		
+		String sql = "select objective_id from objective where type = 'G'\r\n"
+				+ "order by objective_id asc";
+		
+		try {
+			conn = o.connect();
+			pstmt = conn.prepareStatement(sql);	
+			rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				int objective_id = rs.getInt("objective_id");
+				
+				mlist.add(objective_id);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		o.close(conn, pstmt, rs);
+		
+		return mlist;
+	}
+	
 	
 	
 	// 그룹별 미션 성취 여부를 조인을 통해 조회해서 mlist에 저장(최신순)
@@ -196,24 +221,76 @@ public class MissionGroupDAO {
 		return mlist;
 	}	
 	
-public ArrayList<MissionJoinGroupVO> selectMissionGroupChart(int groupid) {
-		
+	public ArrayList<MissionJoinGroupVO> selectMissionGroupChart(int groupid) {
+			
+			DBManager o = OracleDBManager.getInstance();
+			
+			ArrayList<MissionJoinGroupVO> mlist = new ArrayList<MissionJoinGroupVO>();
+			
+			String sql = "SELECT group_id, name, objective_id, success_date, contents, points, COUNT(contents) AS cnt\r\n"
+					+ "FROM (\r\n"
+					+ "    SELECT g.group_id, g.name, s.objective_id, s.success_date, s.contents, s.points\r\n"
+					+ "    FROM (\r\n"
+					+ "        SELECT og.group_id, TO_CHAR(og.success_date, 'YYYY-MM-DD') as success_date, o.objective_id, o.contents, o.points  \r\n"
+					+ "        FROM objective_group og\r\n"
+					+ "        JOIN objective o ON og.objective_id = o.objective_id\r\n"
+					+ "    ) s\r\n"
+					+ "    JOIN groups g ON s.group_id = g.group_id AND g.group_id = ?\r\n"
+					+ ")\r\n"
+					+ "GROUP BY group_id, name, objective_id, success_date, contents, points\r\n"
+					+ "ORDER BY cnt DESC\r\n";
+			
+			try {
+				conn = o.connect();
+				pstmt = conn.prepareStatement(sql);		
+				pstmt.setInt(1, groupid);
+				rs = pstmt.executeQuery();
+				MissionJoinGroupVO mjgv = null;
+				
+				while (rs.next()) {
+					String gname = rs.getString("name");
+					String success_date = rs.getString("success_date");
+					String contents = rs.getString("contents"); // objective_query는 contents로 별칭이 변경됨
+					int group_id = rs.getInt("group_id");
+					int objective_id = rs.getInt("objective_id");
+					int points = rs.getInt("points");
+					int cnt = rs.getInt("cnt");
+					
+					mjgv = new MissionJoinGroupVO();
+					
+					mjgv.setContents(contents);
+					mjgv.setGroup_id(group_id);
+					mjgv.setObjective_id(objective_id);
+					mjgv.setGname(gname);
+					mjgv.setPoints(points);
+					mjgv.setSuccess_date(success_date);
+					mjgv.setCount(cnt);
+					
+					mlist.add(mjgv);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			o.close(conn, pstmt, rs);
+			
+			for (int i = 0; i < mlist.size(); i++) {
+				System.out.println(mlist.get(i).getContents());
+				System.out.println(mlist.get(i).getPoints());
+			}
+			
+			return mlist;
+		}	
+	
+	public ArrayList<MissionJoinGroupVO> selectMissionGroupBadge(int groupid) {
+	
 		DBManager o = OracleDBManager.getInstance();
 		
 		ArrayList<MissionJoinGroupVO> mlist = new ArrayList<MissionJoinGroupVO>();
 		
-		String sql = "SELECT group_id, name, objective_id, success_date, contents, points, COUNT(contents) AS cnt\r\n"
-				+ "FROM (\r\n"
-				+ "    SELECT g.group_id, g.name, s.objective_id, s.success_date, s.contents, s.points\r\n"
-				+ "    FROM (\r\n"
-				+ "        SELECT og.group_id, TO_CHAR(og.success_date, 'YYYY-MM-DD') as success_date, o.objective_id, o.contents, o.points  \r\n"
-				+ "        FROM objective_group og\r\n"
-				+ "        JOIN objective o ON og.objective_id = o.objective_id\r\n"
-				+ "    ) s\r\n"
-				+ "    JOIN groups g ON s.group_id = g.group_id AND g.group_id = ?\r\n"
-				+ ")\r\n"
-				+ "GROUP BY group_id, name, objective_id, success_date, contents, points\r\n"
-				+ "ORDER BY cnt DESC\r\n";
+		String sql = "select g.group_id, g.name, o.contents, o.badge_link, o.points, TO_CHAR(og.success_date, 'YYYY-MM-DD') as success_date\r\n"
+				+ "from groups g, objective_group og, objective o\r\n"
+				+ "where og.objective_id = o.objective_id and g.group_id = og.group_id\r\n"
+				+ "and g.group_id = ?";
 		
 		try {
 			conn = o.connect();
@@ -227,19 +304,17 @@ public ArrayList<MissionJoinGroupVO> selectMissionGroupChart(int groupid) {
 				String success_date = rs.getString("success_date");
 				String contents = rs.getString("contents"); // objective_query는 contents로 별칭이 변경됨
 				int group_id = rs.getInt("group_id");
-				int objective_id = rs.getInt("objective_id");
+				String badge_link = rs.getString("badge_link");
 				int points = rs.getInt("points");
-				int cnt = rs.getInt("cnt");
 				
 				mjgv = new MissionJoinGroupVO();
 				
 				mjgv.setContents(contents);
 				mjgv.setGroup_id(group_id);
-				mjgv.setObjective_id(objective_id);
+				mjgv.setBadgeLink(badge_link);
 				mjgv.setGname(gname);
 				mjgv.setPoints(points);
 				mjgv.setSuccess_date(success_date);
-				mjgv.setCount(cnt);
 				
 				mlist.add(mjgv);
 			}
@@ -251,62 +326,12 @@ public ArrayList<MissionJoinGroupVO> selectMissionGroupChart(int groupid) {
 		for (int i = 0; i < mlist.size(); i++) {
 			System.out.println(mlist.get(i).getContents());
 			System.out.println(mlist.get(i).getPoints());
+			System.out.println(mlist.get(i).getBadgeLink());
 		}
 		
 		return mlist;
 	}	
 	
-public ArrayList<MissionJoinGroupVO> selectMissionGroupBadge(int groupid) {
-	
-	DBManager o = OracleDBManager.getInstance();
-	
-	ArrayList<MissionJoinGroupVO> mlist = new ArrayList<MissionJoinGroupVO>();
-	
-	String sql = "select g.group_id, g.name, o.contents, o.badge_link, o.points, TO_CHAR(og.success_date, 'YYYY-MM-DD') as success_date\r\n"
-			+ "from groups g, objective_group og, objective o\r\n"
-			+ "where og.objective_id = o.objective_id and g.group_id = og.group_id\r\n"
-			+ "and g.group_id = ?";
-	
-	try {
-		conn = o.connect();
-		pstmt = conn.prepareStatement(sql);		
-		pstmt.setInt(1, groupid);
-		rs = pstmt.executeQuery();
-		MissionJoinGroupVO mjgv = null;
-		
-		while (rs.next()) {
-			String gname = rs.getString("name");
-			String success_date = rs.getString("success_date");
-			String contents = rs.getString("contents"); // objective_query는 contents로 별칭이 변경됨
-			int group_id = rs.getInt("group_id");
-			String badge_link = rs.getString("badge_link");
-			int points = rs.getInt("points");
-			
-			mjgv = new MissionJoinGroupVO();
-			
-			mjgv.setContents(contents);
-			mjgv.setGroup_id(group_id);
-			mjgv.setBadgeLink(badge_link);
-			mjgv.setGname(gname);
-			mjgv.setPoints(points);
-			mjgv.setSuccess_date(success_date);
-			
-			mlist.add(mjgv);
-		}
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}
-	o.close(conn, pstmt, rs);
-	
-	for (int i = 0; i < mlist.size(); i++) {
-		System.out.println(mlist.get(i).getContents());
-		System.out.println(mlist.get(i).getPoints());
-		System.out.println(mlist.get(i).getBadgeLink());
-	}
-	
-	return mlist;
-}	
-
 	public ObjectiveVO insertMission(String contents, int points) { //admin용
 		
 		ObjectiveVO uov = new ObjectiveVO();
